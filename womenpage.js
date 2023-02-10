79e329a099894b0ef4d64858d55410b3dfa32156
Samarth <samarthbsacharya@gmail.com>
2023-02-10 20:28:43 +0530

    womenpage

---
+++ womenpage.js
@@ -0,0 +1,415 @@
+let products = [
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/11364258/2020/1/31/899b523a-f57a-48fb-80cd-8422614f195d1580451588210-SASSAFRAS-Women-Green--Beige-Printed-Blouson-Top-49515804515-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Floral Georgette Blouson Top",
+      "price": "674",
+      "MP": "1499",
+      "offer": 55
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/12222044/2020/8/14/16302e01-c0ca-4e70-80e5-db8c2064ce8f1597405258249-SASSAFRAS-Women-Peach-Coloured-Solid-Top-9011597405255592-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "High-Neck Top",
+      "price": "499",
+      "MP": "999",
+      "offer": 50
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/9070021/2019/3/28/f0bce3b7-9ca8-45c9-a33d-dfd07c849a9c1553778513694-SASSAFRAS-Women-Beige--Black-Regular-Fit-Printed-Shirt-11915-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Animal Print Shirt Style Top",
+      "price": "629",
+      "MP": "1399",
+      "offer": 55
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/11679908/2020/5/25/8046704c-bff4-45bc-bf0f-5d438724b7b81590424372490CottinfabWomensBrownPleatedTrouserTopsCarltonLondonWomenTops1.jpg",
+      "brand": "Carlton London",
+      "type": "Victorian Crop Fitted Top",
+      "price": "667",
+      "MP": "1390",
+      "offer": 52
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/12284212/2020/8/25/8fc39488-cee7-4424-870d-ae5bd666d19c1598366814622-SASSAFRAS-Women-Black-Solid-Top-601598366813572-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Ribbed Crop Top",
+      "price": "449",
+      "MP": "899",
+      "offer": 50
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/8643215/2019/2/1/dd3ef60d-be47-4a19-93c9-8598694020281549016416015-SASSAFRAS-Women-Grey-Solid-Blouson-Top-7381549016414303-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Layered Cropped Blouson Top",
+      "price": "499",
+      "MP": "999",
+      "offer": 50
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/1052250/2019/4/11/fb86e884-dbdc-4951-bb45-38cc400c437f1554985706259-SASSAFRAS-Women-Olive-Green-Regular-Fit-Solid-Casual-Shirt-3-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Ruffled Shirt Style Top",
+      "price": "549",
+      "MP": "999",
+      "offer": 45
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/productimage/2021/1/27/c3f87e02-333b-4008-aa5c-3bb5d3fc658a1611743836816-1.jpg",
+      "brand": "Harpa",
+      "type": "Square Neck Top",
+      "price": "643",
+      "MP": "1399",
+      "offer": 54
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/4325708/2018/4/2/11522662991296-Style-Quotient-Women-Tops-4231522662991112-1.jpg",
+      "brand": "Style Quotient",
+      "type": "Polka Dot Print Top",
+      "price": "575",
+      "MP": "1599",
+      "offer": 64
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/8841591/2019/3/5/e92e95ab-68ed-4c6b-8d3e-146d777f9bbf1551780857345-La-Zoire-Stripe-Shifly-Top-831551780856037-1.jpg",
+      "brand": "La Zoire",
+      "type": "Striped A-Line Top",
+      "price": "879",
+      "MP": "1599",
+      "offer": 45
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/13843266/2022/4/18/abaa6501-e1be-4b8e-af96-ecae86eed9e41650283574811SassafrasBlackSilverStripedOneShoulderPuffSleevesCinchedWais1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Striped One Shoulder Puff Sleeves Top",
+      "price": "674",
+      "MP": "1499",
+      "offer": 55
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/9070485/2022/4/18/c2030007-83c4-4a80-8a9e-c964cef044791650284882570SASSAFRASBlackStyledBackCropTop1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Styled Back Crop Top",
+      "price": "499",
+      "MP": "999",
+      "offer": 50
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/13842966/2021/4/3/3d20dde9-8c47-48f2-a079-8cdacd9c78b11617444711671-Sassafras-Brown--Red-Geometric-Printed-Georgette-Wrap-Crop-T-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Geometric Printed Georgette Wrap Top",
+      "price": "674",
+      "MP": "1499",
+      "offer": 55
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/10308651/2019/7/26/09114b19-d977-46ca-8f25-f33c9b47456a1564140179144-SASSAFRAS-Women-Blue-Solid-Top-3621564140176982-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Pleated Frilled Hem Top",
+      "price": "559",
+      "MP": "1399",
+      "offer": 60
+    },
+    {
+      "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/11364152/2020/1/27/091a681c-beb4-4192-bc64-44ca554391141580121893720-SASSAFRAS-Women-White-Solid-Blouson-Top-731580121889856-1.jpg",
+      "brand": "SASSAFRAS",
+      "type": "Extended Sleeved Blouson",
+      "price": "499",
+      "MP": "999",
+      "offer": 50
+    },
+    // edit
+    {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/13962098/2021/4/14/877b6542-dc0b-4352-bf9a-2e200ba068181618405391945DeewaPrintedPinkA-LineDressKurtaSetsZIYAAWomenKurtaSetsZIYAA6.jpg",
+        "brand": "ZIYAA",
+        "type": "Printed Kurta with Trousers",
+        "price": "972",
+        "MP": "2430",
+        "offer": 60
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/14038092/2021/4/20/e70ba29d-ea27-4e0c-990e-38b4838f8e531618917104638KurtaSetsAHIKAWomenKurtaSetsAHIKAWomenKurtaSetsAHIKAWomenKur1.jpg",
+        "brand": "AHIKA",
+        "type": "Printed Kurta with Trousers & Dupatta",
+        "price": "1574",
+        "MP": "3748",
+        "offer": 58
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/productimage/2021/6/2/0f576a65-3225-4aac-9db9-4b5743f9199f1622617345842-1.jpg",
+        "brand": "Stylum",
+        "type": "Women Striped Kurta with Trousers With Dupatta",
+        "price": "1056",
+        "MP": "2400",
+        "offer": 56
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/productimage/2021/6/22/9faa9d87-fb5c-49e0-a748-d3511aa35cd11624340534242-1.jpg",
+        "brand": "Indo Era",
+        "type": "Women Yoke Design Kurta with Palazzos With Dupatta",
+        "price": "1759",
+        "MP": "3999",
+        "offer": 56
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/16295538/2021/11/29/0e6f9fdd-2558-4c03-ab84-df9fbff2d5e91638190875095WomensPinkCottonPrintedKurtaWithTrouserSet1.jpg",
+        "brand": "THE NKS PLUS",
+        "type": "Woman Peach Kurta & Palazzos",
+        "price": "899",
+        "MP": "2499",
+        "offer": 64
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/14203104/2021/6/28/cb46c83c-9d85-4441-bbcb-aa21455b8a861624859278600-Sangria-Women-Kurta-Sets-9871624859277961-1.jpg",
+        "brand": "Sangria",
+        "type": "Women Cotton Kurta Sets",
+        "price": "599",
+        "MP": "1999",
+        "offer": 70
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/productimage/2021/4/9/e9cc12aa-854b-48e0-b7ba-6bcf4e4a62e51617948770143-1.jpg",
+        "brand": "ANAISA",
+        "type": "Embroidered Kurta with Trousers",
+        "price": "987",
+        "MP": "2669",
+        "offer": 63
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/16991432/2022/1/31/d98622c6-c0e9-48d0-8af5-5c6ea78491d61643620795853GoSrikiWomenGreenStraightSolidKurtawithPalazzoDupatta1.jpg",
+        "brand": "GoSriKi",
+        "type": "Kurta with Trousers & Dupatta",
+        "price": "998",
+        "MP": "3699",
+        "offer": 73
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/15262564/2021/10/18/3e5de65a-4aeb-4746-b981-4e3edfcd5fa01634543478338-Indo-Era-Women-Navy-Blue-Ethnic-Motifs-Yoke-Design-Cotton-Ku-13.jpg",
+        "brand": "Indo Era",
+        "type": "Women Yoke Design Kurta with Palazzos With Dupatta",
+        "price": "1403",
+        "MP": "3599",
+        "offer": 61
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/16595430/2022/1/17/e71ccafc-0971-43fc-b6d2-259e9804c34d1642421396636RangritiWomenAssortedPrintedKurtawithPalazzosDupatta1.jpg",
+        "brand": "Rangriti",
+        "type": "Women Assorted Kurta Set",
+        "price": "2299",
+        "MP": null,
+        "offer": null
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/10306657/2019/9/21/185212e8-b3d5-4fee-886d-2aac0dd9e5211569064558443-IMARA-Women-Kurta-Sets-5061569064556675-1.jpg",
+        "brand": "IMARA",
+        "type": "Women Yoke Design Kurta with Trousers & Dupatta",
+        "price": "1499",
+        "MP": "2999",
+        "offer": 50
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/14345986/2022/2/4/2d941a96-a140-4707-8cee-bc3b2c26744b1643937538871NayoWomenNavyBlueStraightScreenPrintPureCottonKurtaPalazzosW1.jpg",
+        "brand": "Nayo",
+        "type": "Women Kurta Set & Dupatta",
+        "price": "1399",
+        "MP": "3499",
+        "offer": 60
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/17171690/2022/2/16/c687fae9-54a6-4c01-af44-771d17fbe6211645005298812KALINIAngrakhaKurtaPantandDupattaSet3.jpg",
+        "brand": "KALINI",
+        "type": "Kurta with Trousers & Dupatta",
+        "price": "999",
+        "MP": "3999",
+        "offer": 75
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/12307502/2022/4/18/ee3d06ff-fe83-4a7f-829a-2c1622987a371650283480461PrakhyaWomenPinkSelfDesignKurtawithTrousersDupatta1.jpg",
+        "brand": "Prakhya",
+        "type": "Self Design Kurta with Trousers & Dupatta",
+        "price": "989",
+        "MP": "3298",
+        "offer": 70
+      },
+      {
+        "image": "https://assets.myntassets.com/h_560,q_90,w_420/v1/assets/images/15508976/2021/9/29/6af853e2-4cf8-45e0-b28a-90c0ba578d0a1632905310824IndoEraBluePrintedStraightKurtaTrouserWithDupattaSet1.jpg",
+        "brand": "Indo Era",
+        "type": "Women Printed Kurta with Palazzos With Dupatta",
+        "price": "2039",
+        "MP": "5999",
+        "offer": 66
+      }
+    
+  ];
+
+  
+window.addEventListener("load", function () {
+    displayproducts(products);
+  });
+
+  function myFunction(){
+    location.reload();
+  }
+
+function displayproducts(products){
+    document.querySelector("#data").innerHTML = "";
+    products.map(function (ele, i){
+        var div = document.createElement('div');
+        var brand = document.createElement("h3");
+        brand.textContent = ele.brand;
+        
+        var image = document.createElement("img");
+        image.setAttribute("src", ele.image)
+        
+        var desc = document.createElement("h3");
+        desc.textContent = ele.type;
+
+        var brandbox = document.createElement("div");
+        brandbox.setAttribute("class","brandbox");
+
+        var pricebox = document.createElement("div");
+        pricebox.setAttribute("class","pricebox");
+
+        var buttonbox =document.createElement("div");
+        buttonbox.setAttribute("class","buttonbox");
+
+
+        var mrp = document.createElement("p");
+        mrp.textContent ="RS."+ele.price;
+        
+        var finalprice = document.createElement("p");
+        finalprice.innerHTML ="RS."+ele.MP;
+
+        var offer=document.createElement("p");
+        offer.textContent="("+ele.offer+"%OFF)";
+        var wishbt=document.createElement("button");
+         wishbt.setAttribute("class","wishbtn");
+        wishbt.innerHTML='<i class="fa-solid fa-heart"></i> whishlist';
+        wishbt.addEventListener("click",function(){
+            addtoWish(i);
+           
+        
+            if(  wishbt.style.backgroundColor=="rgb(83, 87, 102)"){
+              wishbt.style.backgroundColor = "white";
+              wishbt.style.color="#FF69B4";
+             
+              alert("item removed");
+            }
+            else{
+              wishbt.style.color="white";
+              wishbt.style.backgroundColor = "rgb(83, 87, 102)";
+              
+              alert("item added to wishlist");
+            }
+
+
+           
+           console.log( wishbt.style.backgroundColor);
+        });
+        brandbox.append(brand,desc);
+        pricebox.append(mrp, finalprice,offer);
+        buttonbox.append(wishbt);
+        div.append(image,brandbox, pricebox,buttonbox);
+        document.getElementById("data").append(div);
+        
+    });
+    console.log(products);
+    console.log("hello there");
+    var wish = JSON.parse(localStorage.getItem("wishlistGo"))||[];
+    function addtoWish(index){
+        var wishitem = products.filter(function (elem, i){
+            return i=== index;
+        });
+        wish.push(wishitem[0]);
+        console.log(wishitem);
+        console.log("wish-item");
+        localStorage.setItem("wish",JSON.stringify(wish));
+
+    }
+    
+}
+
+console.log("this is the product-page");
+
+function sortvalue(){
+    var sortvalue=document.getElementById("sortit").value;
+    console.log(sortvalue);
+    if(sortvalue=="htl"){
+        products.sort(function(a,b){
+            return b.price-a.price;
+        });
+        displayproducts(products);
+    }
+    if (sortvalue == "lth") {
+        products.sort(function(a,b) {
+          return a.price - b.price;
+        });
+        displayproducts(products);
+      }
+      if(sortvalue =="discount"){
+        products.sort(function(a,b){
+            return b.offer - a.offer;
+        });
+        displayproducts(products);
+      }
+
+      if(sortvalue=="SbyR"){
+        location.reload();
+      }
+      if(sortvalue=="new"){
+        products.sort();
+        products.reverse();
+        displayproducts(products);
+      }
+      if(sortvalue=="Popularity"){
+        products.sort(function(){return 0.5 - Math.random()});
+        displayproducts(products);
+      }
+}
+
+const checkboxes = document.querySelectorAll('input[type="checkbox"]');
+ 
+  checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener('change', function() {
+     let filteredArray = [];
+      checkboxes.forEach(function(cb) {
+        if (cb.checked) {
+          filteredArray = [...filteredArray, ...products.filter(function(obj) {
+            return obj.brand === cb.value;
+            console.log(obj.brand,cb.value);
+         
+          })];
+
+        }
+      });
+
+      // to edit
+    //   checkboxes.forEach(function(cb) {
+    //     if (cb.checked) {
+    //       filteredArray = [...filteredArray, ...products.filter(function(obj) {
+    //         return obj.color === cb.value;
+    //         console.log(obj.brand,cb.value);
+         
+    //       })];
+          
+    //     }
+    //   });
+
+      // to edit
+
+      // to edit
+    
+      if(filteredArray.length===0){
+          displayproducts(products);
+      }
+      else{
+          displayproducts(filteredArray);
+      }
+    });
+  });
+
+
+
+
+  
